0be5c64f8f4ac8f2687d6800f7c308d23b2bd7de
RickyStanziale22 <rickyrichidou@gmail.com>
2022-06-08 16:43:12 -0300

    categoria de productos

--- 1/categoria_de_productos/main.js
+++ 1/categoria_de_productos/main.js
@@ -1,24 +1,28 @@
-const consumir = async (cat) =>{
+const obtener_productos = async (dato) =>{
 
-    const datos = await fetch('https://fakestoreapi.com/products/categories')
-    
-    const datos_definidos = await datos.json()
-    
+    const datardos = await fetch("https://fakestoreapi.com/products/category/"+dato)
+    const elementos = await datardos.json()
+
     let columnas = []
-    
-    datos_definidos.forEach(element => {
-       
-        let columna =
-        `<tr>
-        <td> <img src="${element.image}" class="img-fluid" style= "width:4rem"></td>
-        <td>${element.name}</td>
-        <td>${element.symbol}</td>
-        <td>${element.current_price}</td>
-        <td>${element.total_volume}</td>
-        </tr>`
-    
+
+    elementos.forEach(element => {
+        let columna = 
+        `
+        <div class="col-lg-3">
+            <div class="card">
+                <img height="300" src="${element.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text">$${element.price}</p>
+                    <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+            </div>
+        </div>`
+
         columnas.push(columna)
+
     });
-    document.getElementById("menem").innerHTML = columnas.join('')
-    }
-    consumir()
+
+    document.getElementById("catalogo").innerHTML = columnas.join('')
+
+}
